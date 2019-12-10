e944243b7c9c4dc35d4e643e36220679d399bb43
Patryk <gustaw5022@gmail.com>
2019-12-10 23:08:21 +0100

    created authorDao methods

--- src/main/java/com/patryk/booksrental/DAO/AuthorDao.java
+++ src/main/java/com/patryk/booksrental/DAO/AuthorDao.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface AuthorDao {
     List<Author> getList();
+    Author getAuthorById(long id);
 }
--- src/main/java/com/patryk/booksrental/DAO/AuthorDaoImpl.java
+++ src/main/java/com/patryk/booksrental/DAO/AuthorDaoImpl.java
@@ -1,4 +1,26 @@
 package com.patryk.booksrental.DAO;
 
-public class AuthorDaoImpl {
+import com.patryk.booksrental.Models.Author;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class AuthorDaoImpl implements AuthorDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<Author> getList() {
+        return entityManager.createQuery("select a from Authors a").getResultList();
+    }
+
+    @Override
+    public Author getAuthorById(long id) {
+        return entityManager.createQuery("Select a from Authors a where a.id = :authorId", Author.class)
+                .setParameter("authorId", id).getSingleResult();
+    }
 }
