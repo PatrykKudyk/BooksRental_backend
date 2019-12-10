a788346de03007c6b8947022cc2e2622f82f0910
Patryk <gustaw5022@gmail.com>
2019-12-10 17:02:16 +0100

    initial commit

---
+++ src/test/java/com/patryk/booksrental/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patryk.booksrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
