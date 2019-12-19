515c50b11c55b22393aedd6f0e18358381af8334
Patryk <gustaw5022@gmail.com>
2019-12-19 17:25:25 +0100

    expanded location API

--- src/main/java/com/patryk/booksrental/API/Assemblers/LocationAssembler.java
+++ src/main/java/com/patryk/booksrental/API/Assemblers/LocationAssembler.java
@@ -27,4 +27,20 @@ public class LocationAssembler {
         }).collect(Collectors.toList());
         return locationResourceList;
     }
+
+    public LocationResource buildResource(Location location){
+        LocationResource locationResource = LocationResource.builder()
+                .id(location.getId())
+                .state(location.getState())
+                .city(location.getCity())
+                .zip_code(location.getZip_code())
+                .street(location.getStreet())
+                .building_number(location.getBuilding_number())
+                .office_number(location.getOffice_number())
+                .is_used(location.is_used())
+                .publishingHouse(location.getPublishing_house())
+                .rental(location.getRental())
+                .build();
+        return locationResource;
+    }
 }
--- src/main/java/com/patryk/booksrental/API/Controllers/LocationController.java
+++ src/main/java/com/patryk/booksrental/API/Controllers/LocationController.java
@@ -6,9 +6,9 @@ import com.patryk.booksrental.API.Resources.LocationResource;
 import com.patryk.booksrental.Models.Location;
 import com.patryk.booksrental.Services.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +22,19 @@ public class LocationController {
     public List<LocationResource> getList(){
         List<Location> locationList = locationService.getList();
         return locationAssembler.buildResources(locationList);
+    }
+
+    @RequestMapping(value = "/{someID}", produces = "application/json", method = RequestMethod.GET)
+    public LocationResource getLocationById(@PathVariable(value = "someID") long id){
+        Location location = locationService.getLocationById(id);
+        return locationAssembler.buildResource(location);
+    }
+
+    @RequestMapping(value = "/updateUsage", method = RequestMethod.PUT)
+    public ResponseEntity<String> updateUsage(@RequestParam(required = true)long id,
+                                              @RequestParam(required = true)boolean is_used){
+        locationService.updateUsage(id, is_used);
+        return new ResponseEntity<>("Zmiana dokonana pomyślnie!", HttpStatus.OK);
     }
 
 
