3782088e5c4ee9a870dc5d934e8bc1ab03fd5ea9
Patryk <gustaw5022@gmail.com>
2020-01-12 14:09:25 +0100

    added user service

---
+++ src/main/java/com/patryk/booksrental/Services/UserServiceImpl.java
@@ -0,0 +1,57 @@
+package com.patryk.booksrental.Services;
+
+import com.patryk.booksrental.DAO.UserDao;
+import com.patryk.booksrental.Models.User;
+import com.patryk.booksrental.Models.UserType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private UserDao userDao;
+
+    @Override
+    public List<User> getList() {
+        return userDao.getList();
+    }
+
+    @Override
+    public User getUsetById(long id) {
+        return userDao.getUsetById(id);
+    }
+
+    @Override
+    public User getUserByLogin(String login) {
+        return userDao.getUserByLogin(login);
+    }
+
+    @Override
+    public void addUser(User user) {
+        userDao.addUser(user);
+    }
+
+    @Override
+    public void updatePassword(long id, String password) {
+        userDao.updatePassword(id, password);
+    }
+
+    @Override
+    public void updateEmail(long id, String email) {
+        userDao.updateEmail(id, email);
+    }
+
+    @Override
+    public void updateUserType(long id, UserType userType) {
+        userDao.updateUserType(id, userType);
+    }
+
+    @Override
+    public void updateUser(long id, User user) {
+        userDao.updateUser(id, user);
+    }
+
+    @Autowired
+    public void setUserDao(UserDao userDao){this.userDao = userDao;}
+}
